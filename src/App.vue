c96edb19eaf7bfa54500309e2cb4a5c9c62fa82a
espaghettion <krystofkremecek@gmail.com>
2024-11-04 19:10:14 +0100

    added base design

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,103 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const active = ref(false);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <section>
+      <img class="logo" src="./assets/logo.svg" alt="">
+      <p>Řecká mytologie</p>
+    </section>
+    <nav>
+      <button @click="active = !active"><i class="fa-solid fa-bars"></i></button>
+    </nav>
   </header>
+  <article class="menu" :class="{ 'active': active }">
+    <RouterLink class="link" to="/">MYTOLOGIE</RouterLink>
+    <RouterLink class="link" to="/">BOHOVÉ</RouterLink>
+    <RouterLink class="link" to="/">TITÁNI</RouterLink>
+    <RouterLink class="link" to="/">HRDINOVÉ</RouterLink>
+    <RouterLink class="link" to="/">NADLIDSKÉ STVOŘENÍ</RouterLink>
+    <RouterLink class="link" to="/about">O STRÁNCE</RouterLink>
+  </article>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  width: 100%;
+  background-color: #080C37;
+  background-image: url(./assets/feather-texture.png);
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  width: 60px;
+}
+
+section{
+  display: flex;
+  align-items: center;
+  width: 40%;
+  color: white;
+  font-size: 2.5em;
+  font-weight: 600;
+  padding: 10px;
+  gap: 20px;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  width: 60%;
+  padding: 10px;
+}
+
+button{
+  width: 40px;
+  height: 40px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border: 3px solid #ACAFCC;
+  border-radius: 5px;
+  color: #ACAFCC;
+  font-size: 1.3em;
+
+  :hover{
+    cursor: pointer;
+  }
+}
+
+.menu{
+  display: none;
+  width: 220px;
+  padding: 10px;
+  position: absolute;
+  right: 0px;
+  background-color: #FFF5E0;
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.25);
   text-align: center;
-  margin-top: 2rem;
+  font-weight: 600;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.link{
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 5px;
+  width: 200px;
+  color: white;
+  background: linear-gradient(0deg, rgba(14,26,143,1) 0%, rgba(8,12,49,1) 100%);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.active{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
 }
 </style>
